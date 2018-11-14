dfb10527f35bf13655b6af41cda135bd7fb11eac
Matt Pilott <matt@creativelittledots.co.uk>
2018-11-14 16:23:30 +0000

    Initial

---
+++ src/client.js
@@ -0,0 +1,52 @@
+import * as sapper from '../__sapper__/client.js';
+import { Store } from 'svelte/store.js';
+
+function post(endpoint, data) {
+
+	return fetch(endpoint, {
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(r => r.json());
+}
+
+class AuthStore extends Store {
+
+	login(credentials) {
+		return post(`auth/login`, credentials).then(response => {
+			if (response.user) this.set({ user: response.user });
+			return response;
+		});
+	}
+
+	logout() {
+		return post(`auth/logout`).then(response => {
+			this.set({ user: null });
+			return response;
+		});
+	}
+
+	register(user) {
+		return post(`auth/register`, user).then(response => {
+			if (response.user) this.set({ user: response.user });
+			return response;
+		});
+	}
+
+	save(user) {
+		return post(`auth/save`, user).then(response => {
+			if (response.user) this.set({ user: response.user });
+			return response;
+		});
+	}
+}
+
+sapper.start({
+	target: document.querySelector('#sapper'),
+	store: data => {
+		return new AuthStore(data);
+	}
+});
---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import compression from 'compression';
+import serve from 'serve-static';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+import * as sapper from '../__sapper__/server.js';
+import { Store } from 'svelte/store.js';
+
+const FileStore = sessionFileStore(session);
+
+express()
+	.use(bodyParser.json())
+	.use(session({
+		secret: 'topsecret',
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 31536000
+		},
+		store: new FileStore({
+			path: process.env.NOW ? `/tmp/sessions` : `.sessions`
+		})
+	}))
+	.use(
+		compression({ threshold: 0 }),
+		serve('static'),
+		sapper.middleware({
+			store: req => {
+				return new Store({
+					user: req.session && req.session.user
+				});
+			}
+		})
+	)
+	.listen(process.env.PORT);
