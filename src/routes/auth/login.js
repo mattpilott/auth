0d57b3c6c43daf5f408c2d0127e1c36014bf2d24
Matt Pilott <matt@creativelittledots.co.uk>
2019-04-25 16:15:08 +0100

    .env

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -1,29 +1,30 @@
 import * as api from '../_api.js';
 import send from '@polka/send-type';
+import 'dotenv/config';
 
 export function post(req, res) {
 
     const userCredentials = req.body;
     const clientCredentials = {
         grant_type: 'password',
-        client_id: 'hq23usojfYa3Sz3HhTMhMDbuGpv7foN5QZnlBluy',
-        client_secret: 'UIWPzxSAchB1Y75LFEPqh0ekIQfbDpNOC4uiMbe8'
+        client_id: process.env.LOGIN_ID,
+        client_secret: process.env.LOGIN_SECRET
     };
 
     const allCredentials = {...clientCredentials, ...userCredentials};
 
-	api.post('/wp/?oauth=token', allCredentials)
+	api.post('wp/?oauth=token', allCredentials)
         .then(token => {
 
             if ( token.access_token ) {
 
         		req.session.token = token;
 
-                api.post('/wp-json/wp/v2/users/me', null, token.access_token)
+                api.post('wp-json/wp/v2/users/me', null, token.access_token)
                     .then(user => {
 
                         req.session.user = user;
-                        
+
                 		res.end(JSON.stringify({...user, token: token.access_token}));
 
                     })
--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,23 +1,24 @@
 import * as api from '../_api.js';
 import send from '@polka/send-type';
+import 'dotenv/config';
 
 export function post(req, res) {
 
     const userCredentials = req.body;
     const clientCredentials = {
         grant_type: 'client_credentials',
-        client_id: 'bRj4jQPZmbISw6J0E17tO6Bo3kVOXAtYu3COpJvo',
-        client_secret: 'K2DyNVSOiB68WIjzYeteqVlwEqZw5GmjYooZpa1l'
+        client_id: process.env.REGISTER_ID,
+        client_secret: process.env.REGISTER_SECRET
     };
 
-	api.post('/wp/?oauth=token', clientCredentials)
+	api.post('wp/?oauth=token', clientCredentials)
         .then(token => {
 
             if ( token.access_token ) {
 
                 req.session.token = token;
 
-                api.post('/wp-json/wp/v2/users', userCredentials, token.access_token)
+                api.post('wp-json/wp/v2/users', userCredentials, token.access_token)
                     .then(user => {
 
                 		res.end(JSON.stringify(user));
